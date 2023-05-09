789d6c5d24bdf0a0de889ae856648dff1dc2ec14
Drew-bozarth <dbozarth@chapman.edu>
2023-05-09 11:07:28 -0700

    adding comments to server side

--- readme.md
+++ readme.md
@@ -1,1 +1,36 @@
-A new repo
+# Technical-Take-Home Assigment
+
+A repo containing the server side of the project.
+
+### Drew Bozarth
+
+### dbozarth@chapman.edu
+
+## Source Files:
+
+### index.js
+
+## Known Errors:
+
+###
+
+## References:
+
+### Arpan Neupane - https://www.youtube.com/watch?v=w3vs4a03y3I - video on a React project with Express and NodeJS.
+
+### PedroTech - https://www.youtube.com/watch?v=NU-HfZY3ATQ - video on Socket.io tutorials with ReactJS and NodeJS.
+
+## Instructions:
+
+### Required installations in server folder:
+
+```
+npm init
+npm install cors express nodemon socket.io
+```
+
+### To start the server, run:
+
+```
+npm start
+```
--- server/index.js
+++ server/index.js
@@ -1,14 +1,23 @@
+// setup express
 const express = require("express");
 const app = express();
+// need http library to build the server with socket.io
 const http = require("http");
+// cors is a library that will allow us to use the cors middleware and resolve a lot of issues
 const cors = require("cors");
+// import the class Server from socket.io library
 const { Server } = require("socket.io");
+// import the Users class from the Users.js file
 const { Users } = require("../client/src/Users");
 app.use(cors());
 let users = new Users();
 
+// pass in the express app and the we can listen to the server
 const server = http.createServer(app);
 
+// create an io connection for the server
+// use object with cors to setup settings for the origin of the 
+//    socket and which requests are accepted
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
@@ -16,9 +25,12 @@ const io = new Server(server, {
   },
 });
 
+// on a connection to the socket (when a user loads the app on localhost:3000)
 io.on("connection", (socket) => {
+  // print to console the socket.id to check the user loaded 
   console.log(`User Connected: ${socket.id}`);
 
+  // connects the user to a room and passes the username to update the list of online users
   socket.on("join_room", (username, room) => {
     socket.join(room);
     users.addUser(socket.id,username,room);
@@ -26,10 +38,13 @@ io.on("connection", (socket) => {
     console.log(`User with ID: ${socket.id} Name: ${username}joined room: ${room}`);
   });
 
+  // send message will emit the message data to the specified room
   socket.on("send_message", (data) => {
     socket.to(data.room).emit("receive_message", data);
   });
 
+  // when a user disconnects, it will remove them from the list we are keeping
+  //  and it will update that list so the readouts are correct on screen
   socket.on("disconnect", () => {
     let user = users.removeUser(socket.id);
     if (user) {
@@ -39,6 +54,7 @@ io.on("connection", (socket) => {
   });
 });
 
+// listen on port 3001 (react runs on 3000)
 server.listen(3001, () => {
   console.log("Server listening on Port 3000");
 });
